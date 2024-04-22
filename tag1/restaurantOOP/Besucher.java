334113c56cb7e4c98cde70d014429b5b2f488ab6
Friedrich August von Hayek <fahayek@postcashcash.com>
2024-04-22 16:30:31 +0200

    feierabend

---
+++ tag1/restaurantOOP/Besucher.java
@@ -0,0 +1,13 @@
+package tag1.restaurantOOP;
+
+//Erstelle eine Klasse für einen Besucher. Diese soll als Attribute die Tischnummer, eine Liste für
+//die bestellten Gerichte und Getränke (die zunächst leer ist) und den Rechnungsbetrag (der zu
+//Beginn bei 0 Euro liegt) enthalten. Verwende für den Konstruktor daher nur die Tischnummer als
+//Übergabewert und gib die übrigen Attribute direkt vor.
+
+
+public class Besucher {
+    Besucher(int tischnummer, String[] bestellungen, int rechnungsbetrag) {
+
+    }
+}
--- tag1/restaurantOOP/Main.java
+++ tag1/restaurantOOP/Main.java
@@ -1,4 +1,5 @@
 package tag1.restaurantOOP;
 
 public class Main {
+    Besucher maike = new Besucher()
 }
